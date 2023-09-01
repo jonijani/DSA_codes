6c865b663bdd96e3a9fbb5eac7bec28306662991
jonijani <junaidzaib.web@gmail.com>
2023-09-01 20:42:10 +0100

    practiced and created find_pair_unsorted_list

--- DSA_practice/2_pointer_unsorted.py
+++ DSA_practice/2_pointer_unsorted.py
@@ -3,7 +3,7 @@ import time
 
 def get_pair_from_unsorted_arr(arr,t): # brute force approach time complexity O(n**2)
     l = []
-    for i in range(0,len(arr)-1):
+    for i in range(0,len(arr)):
         for j in range(i+1,len(arr)):
            # time.sleep(0.5)
             print(arr[i],arr[j])
@@ -18,9 +18,9 @@ def get_pair_from_unsorted_arr(arr,t): # brute force approach time complexity O(
 
 
 
-if _name=='main_':
+if __name__=='__main__':
     arr = [0,3,5,1,2,9,6,15,10]
-    t = 26
+    t = 15
 
     result = get_pair_from_unsorted_arr(arr,t)
     print(result)
--- DSA_practice/practice.py
+++ DSA_practice/practice.py
@@ -4,36 +4,62 @@
 import time
 
 # not good approach 
-def find_pair_of_number_brute_force(arr,x): # brute force approach  O(n*n) quadratic complexity   
+'''def find_pair_of_number_brute_force(arr,x): # brute force approach  O(n*n) quadratic complexity   
     for i in range(0,len(arr)-1):
         for j in range(i+1,len(arr)):
             print(arr[i],arr[j])
             time.sleep(0.5)
             if arr[i] + arr[j] == x:
-                return arr[i],arr[j]
+                return arr[i],arr[j]'''
             
-def find_pair_of_num_using_two_pointer(arr,x): # O(n) liner complexity
+
+
+# def find_pair_of_num_using_two_pointer(arr,x): # O(n) liner complexity
+#     f = 0
+#     b = len(arr)-1
+#     while f < b:
+#         print(arr[f],arr[b])
+#         time.sleep(0.5)
+#         sum_ = arr[f] + arr[b]
+#         if sum_ == x:
+#             return arr[f],arr[b]
+#         elif sum_ < x:
+#             f += 1
+#         else:  # sum_ > x
+#             b -= 1
+
+
+# arr = [2, 3, 5, 8, 11]
+
+def find_pair_sorted_list(arr,t):
+    s_arr = sorted(arr)
     f = 0
-    b = len(arr)-1
+    b = len(s_arr)-1
     while f < b:
-        print(arr[f],arr[b])
+        print(s_arr[f],s_arr[b])
         time.sleep(0.5)
-        sum_ = arr[f] + arr[b]
-        if sum_ == x:
-            return arr[f],arr[b]
-        elif sum_ < x:
+        sum_ = s_arr[f] + s_arr[b]
+        if sum_ == t:
+            return s_arr[f], s_arr[b]
+        elif sum_ < t:
             f += 1
-        else:  # sum_ > x
-            b -= 1
+        else:
+
+            b = b-1
+    return None
+
+
 
         
 
 if __name__=='__main__':
-    arr = [2,4,8,12,15,20,21,22,45,67,87,98,111,121,122,123,124,125,126,345,400]
-    x = 745
+    #arr = [2,4,8,12,15,20,21,22,45,67,87,98,111,121,122,123,124,125,126,345,400]
+    arr = [3,5,2,8,11]
+    x = 16
 
     #print(find_pair_of_number_brute_force(arr,x))
-    print(find_pair_of_num_using_two_pointer(arr,x))
+    #print(find_pair_of_num_using_two_pointer(arr,x))
+    print(find_pair_sorted_list(arr,x))
 
 
 
